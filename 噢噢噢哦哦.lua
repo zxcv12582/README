db998c065c2378ef523b6f561fc9a33470bdf1ca
zxcv12582 <2759331946@qq.com>
2025-07-17 04:43:43 +0800

    Add files via upload

--- 噢噢噢哦哦.lua
+++ 噢噢噢哦哦.lua
@@ -4,10 +4,12 @@ local window = library:new("殺脚本(测试版)")--脚本名字
 ----------------------------------------------------------------------------------------------------------------------------------------
 
 
-local creds = window:Tab("文字",'6031097229')--文件夹
+local creds = window:Tab("通用",'6031097229')--文件夹
 
 local credits = creds:section("文字",true)--文件
 
-credits:Button("文字",function()
-    脚本链接
+credits:Button("通用
+",function(）
+	Callback = function()	loadstring(game:HttpGet("https://pastefy.app/8B4iDhvf/raw"))()
+	
 end)--功能
