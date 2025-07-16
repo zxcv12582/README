a58664aa2ceecaea19da0577b577d271db050d47
zxcv12582 <2759331946@qq.com>
2025-07-17 04:50:01 +0800

    Add files via upload

---
+++ 噢噢噢哦哦哦哦.lua
@@ -0,0 +1,15 @@
+local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/FengY4/FengY4/refs/heads/main/Fengui.lua", true))()
+----------------------------------------------------------------------------------------------------------------------------------------UI
+local window = library:new("殺脚本(测试版)")--脚本名字
+----------------------------------------------------------------------------------------------------------------------------------------
+
+
+local creds = window:Tab("通用",'6031097229')--文件夹
+
+local credits = creds:section("文字",true)--文件
+
+credits:Button("通用
+",function(）
+	Callback = function()	loadstring(game:HttpGet("https://pastefy.app/8B4iDhvf/raw"))()
+	
+end)--功能
